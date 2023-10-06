340e9dcce4b9e7d0a7c1db71e357c3d896735e11
andreadityayanwar <andreadityayanwar@gmail.com>
2023-10-06 16:12:35 +0700

    add: dynamic form

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -7,6 +7,7 @@ import {useForm, Controller, SubmitHandler} from "react-hook-form"
 
 
 interface Purchasing  {
+    id?: number
     itemName : {value: number; label: string}
     price: number
     qty: number
@@ -15,12 +16,39 @@ interface Purchasing  {
 }
 
 const Dashboard = () => {
-    const {control, handleSubmit} = useForm({
-        defaultValues: {
+    var plusID: number = 0
+    const [addIngredients, setAddIngredient] = React.useState([
+        {
+            id: plusID ++,
             itemName: {},
             price: 0,
             qty: 0,
             discount: 0,
+            total:0,
+        }
+    ])
+
+    const addIngredientRow = () => {
+        let _addIngredients = [...addIngredients]
+        _addIngredients.push({
+            id: plusID ++,
+            itemName: {},
+            price: 0,
+            qty: 0,
+            discount: 0,
+            total:0,
+        })
+        setAddIngredient(_addIngredients)
+    }
+    
+    const {control, handleSubmit} = useForm({
+        defaultValues: {
+            id: plusID ++,
+            itemName: {},
+            price: 0,
+            qty: 0,
+            discount: 0,
+            total:0,
         },
     })
     const onSubmit : SubmitHandler<Purchasing> = (data) => {console.log(data)}
@@ -29,28 +57,30 @@ const Dashboard = () => {
         <>
         <div className="px-5 pt-4">
             <div className="border-gray-900/10 pb-12">
-                <h2 className="text-lg font-semibold leading-7 text-gray-900">Purchasing</h2>
+                <h2 className="text-lg font-semibold leading-7 text-gray-900">Ingredient</h2>
                 <form onSubmit={handleSubmit(onSubmit)}>
-                    <div className="grid col-span-full pt-2">
-                        <label htmlFor="itemName" className="block text-sm font-medium leading-6 text-gray-900">
-                            Item Name
-                        </label>
+                    {addIngredients.map((ingredient) => (
+                        <div className="grid grid-cols-6 col-start-1 gap-2 pt-2" key={ingredient.id}>
                         <div className="">
-                            <Controller
-                                name="itemName"
-                                control={control}
-                                render= {({field}) => (
-                                    <Select
-                                        {...field}
-                                        options={[
-                                            {value: 1, label: "Milk"},
-                                            {value: 2, label: "Coffe"},
-                                            {value: 3, label: "Juice"},
-                                        ]}/>
-                                )}/>
+                            <label htmlFor="itemName" className="block text-sm font-medium leading-6 text-gray-900">
+                                Item Name
+                            </label>
+                            <div className="">
+                                <Controller
+                                    name="itemName"
+                                    control={control}
+                                    render= {({field}) => (
+                                        <Select
+                                            {...field}
+                                            options={[
+                                                {value: 1, label: "Milk"},
+                                                {value: 2, label: "Coffe"},
+                                                {value: 3, label: "Juice"},
+                                            ]}/>
+                                    )}
+                                />
                         </div>
                     </div>
-                    <div className="grid grid-cols-4 col-start-1 gap-2 pt-2">
                         <div>
                             <label htmlFor="price" className="block text-sm font-medium leading-6 text-gray-900">
                                 Price
@@ -67,10 +97,7 @@ const Dashboard = () => {
                                 QTY
                             </label>
                             <div className="">
-                                <Controller
-                                    name="qty"
-                                    control={control}
-                                    render={({field}) => <input className='block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6' {...field}/>}/>
+                                <input className='block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6' />
                             </div>
                         </div>
                         <div>
@@ -88,8 +115,18 @@ const Dashboard = () => {
                             <label htmlFor="total" className="block text-sm font-medium leading-6 text-gray-900">
                                 Total
                             </label>
+                            <div className="">
+                                <Controller
+                                    name="total"
+                                    control={control}
+                                    render={({field}) => <input className='block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6' {...field} disabled/>}/>
+                            </div>
+                        </div>
+                        <div className='flex flex-wrap content-end'>
+                            <button onClick={addIngredientRow} className='btn btn-sm btn-active btn-info text-white'>+</button>
                         </div>
                     </div>
+                    ))}
                     <div className='pt-3'>
                     <button type="submit" className='btn btn-sm btn-active btn-neutral'>Save</button>
                     </div>
